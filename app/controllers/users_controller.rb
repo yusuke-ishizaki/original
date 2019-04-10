a0915ab44fecd85e645c1be4585b98a31c26cb51
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-10 16:18:06 +0900

    useer_name 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def show
-        @posts = Post.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+        @name = current_user.name
+        @posts = current_user.posts.page(params[:page]).per(5).order("created_at DESC")
     end
 end

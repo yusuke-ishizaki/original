67c283f9060161d6d11be668717675fdc716106a
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-11 18:43:48 +0900

    hitodanraku
    ここまで

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :tags, dependent: :destroy
+    validates :title, presence: true
+    validates :text, presence: true
 end
--- db/migrate/20190410050120_create_posts.rb
+++ db/migrate/20190410050120_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title, null: false
-      t.text :text, null: false
+      t.string :title
+      t.text :text
       t.binary :image
 
       t.timestamps

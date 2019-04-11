37a8f303a82a4f4e86ba1606bd930fceea06b320
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-12 04:58:17 +0900

    tag_2
    show実装

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,5 +5,31 @@ class TagsController < ApplicationController
     def new
         @tag = Tag.new
     end
+    def index
+        @tags = []
+        Tag.order("name ASC").each do |tag|
+          i = 0
+          @tags.each do |t|
+            if tag.name != t.name
+              i += 1
+            end
+          end
+          if i == @tags.length
+            @tags << tag
+          end
+        end
+      end
     
+      def show
+        @tag = Tag.find(params[:id])
+        @posts = []
+        Post.all.each do |post|
+          post.tags.each do |tag|
+            if tag.name == @tag.name
+              @posts << post
+              break
+            end
+          end
+        end
+      end
 end

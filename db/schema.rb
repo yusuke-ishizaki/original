ad19f06863bbd0c9e3121c54ca569f08a1194ef5
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-12 13:41:38 +0900

    Twitter,facebook

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_223354) do
+ActiveRecord::Schema.define(version: 2019_04_12_042452) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2019_04_10_223354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "twitter"
+    t.string "facebook"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
